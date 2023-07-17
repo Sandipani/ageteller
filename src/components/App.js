5e7cb4efc5cc637c900cda86e4628fe6dc1ba10e
Sandipani <biswas.sandipani@gmail.com>
2023-07-17 14:07:46 +0000

    Changes to be committed: 	new file:   src/components/AgeStats.js 	modified:   src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
 import { Form,FormControl, Button } from 'react-bootstrap';
 import './App.css';
+//import AgeStats from './AgeStats';
 
 class App extends Component {
+    constructor()
+    {
+        super();
+
+        this.State = 
+        {
+          newDate: ' ',
+          birthday:'1999-07-28'
+        }
+    }
+
+changeBirthday(){
+    console.log(this.state);
+    this.setState({birthday: this.state.newDate});
+}
+
     render () {
         return (
         <div className="App">
             <Form inline>
                 <h2>Enter your birthday !!!</h2>
-                <FormControl type="date">
+                <FormControl 
+                    type="date"
+                    onChange= {event => this.setState({newDate: event.target.value})} 
+                >      
                 </FormControl>
                 {' '}
-                <Button>
+                <Button
+                onClick={()=> this.changeBirthday()}
+                >
                     Submit
                 </Button>
+                
             </Form>
         </div>
         )
