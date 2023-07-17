66698f35cc4867b698cd521edb68bfc490068ca4
Sandipani <biswas.sandipani@gmail.com>
2023-07-17 12:56:41 +0000

    Changes to be committed: 	new file:   src/components/App.css 	modified:   src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
+import { Form,FormControl, Button } from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
     render () {
-        return <div>ageteller component</div>
+        return (
+        <div className="App">
+            <Form inline>
+                <h2>Enter your birthday !!!</h2>
+                <FormControl type="date">
+                </FormControl>
+                {' '}
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+        )
     }
 }
 
